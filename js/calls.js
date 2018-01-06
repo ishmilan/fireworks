22842e9633e16247850aa0668181b69db037c359
ishmilan <ish.milan.dev@gmail.com>
2018-01-06 20:03:07 +0100

    documentation

--- js/calls.js
+++ js/calls.js
@@ -6,5 +6,21 @@ function muestraMenu() {
 function muestraFooter() {
 	display('footer');
 }
+document.getElementById("btn-icomoon").onclick=muestraIcomoon;
+document.getElementById("btn-feather").onclick=muestraFeather;
+document.getElementById("btn-glyphicon").onclick=muestraGlyphicon;
+document.getElementById("btn-fontawesome").onclick=muestraFontawesome;
+function muestraIcomoon() {
+	toggle('#container-icomoon','hidden');
+}
+function muestraFeather() {
+	toggle('#container-feather','hidden');
+}
+function muestraGlyphicon() {
+	toggle('#container-glyphicons','hidden');
+}
+function muestraFontawesome() {
+	toggle('#container-fontawesome','hidden');
+}
 imgTransition("imgContainer01",2);
 imgTransition("imgContainer02",3);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+"use strict";
+var fw = {};
+fw.setCookie = function(name, value, exdays){
+	if(!(name && value)) return false;
+	if(!exdays) exdays=1;
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+ d.toUTCString();
+	document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
 function display(item1){
 	if (document.getElementById(item1).style.display == "block"){
 		document.getElementById(item1).style.display = "none";
@@ -5,6 +16,16 @@ function display(item1){
 		document.getElementById(item1).style.display = "block";
 	}
 };
+function toggle(element,nameOfClass){
+	if(!(element && nameOfClass)) return false;
+	var e = document.querySelectorAll(element);
+	if(!e) return false;
+	var length = e.length;
+	for (var i = 0; i < length; i++) {
+		e[i].classList.toggle(nameOfClass);
+	}
+	return true;
+}
 function imgTransition(id,time){
 	var slideIndex =0;
 	slideImg();
@@ -14,8 +35,16 @@ function imgTransition(id,time){
 			imgs[i].style.display = "none";
 		}
 		slideIndex++;
-		if (slideIndex > imgs.length) {slideIndex = 1} 
+		if (slideIndex > imgs.length) {slideIndex = 1}
 		imgs[slideIndex-1].style.display = "block";
 		setTimeout(slideImg,time*1000);
 	}
 };
+window.addEventListener("load",function(){
+	var close = document.querySelectorAll("[data-close]");
+	for (var i = 0; i < close.length; i++) {
+		close[i].addEventListener("click",function(){
+			this.parentNode.style.display="none";
+		});
+	}
+});
